5107e8fd8c1691a05b4b5d6129d268e6b8d7933a
GITA <jgitanjali592@gmail.com>
2024-04-08 14:25:45 +0530

    this code for aboutus components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans"><a href="#home">Home</a></li>
-        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#about">About Us</a></li>
         <li className="p__opensans"><a href="#menu">Menu</a></li>
         <li className="p__opensans"><a href="#awards">Awards</a></li>
         <li className="p__opensans"><a href="#contact">Contact</a></li>
@@ -30,7 +30,7 @@ const Navbar = () => {
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
               <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
-              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About Us</a></li>
               <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
               <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
               <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
